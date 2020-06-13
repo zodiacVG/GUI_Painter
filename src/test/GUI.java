b828eec145798bb75de7b10970dd69f8d574d671
zodiacVG <zhangzibo418@outlook.com>
2020-06-13 20:46:10 +0800

    Initial commit

---
+++ src/test/GUI.java
@@ -0,0 +1,10 @@
+package test;
+import javax.swing.*;
+public class GUI extends JFrame{
+    JPanel toolp;    //¹¤¾ßÀ¸
+    JButton fileChoose;
+    JButton colorCHOOSE;
+    public static void main(String[] args){
+
+    }
+}
